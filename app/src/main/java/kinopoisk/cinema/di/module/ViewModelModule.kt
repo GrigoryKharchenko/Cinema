266d6c9aa8c47c097d6b19f9765a239710e0e3bc
GrigoryKharchenko <96061286+GrigoryKharchenko@users.noreply.github.com>
2022-12-28 18:39:36 +0600

    Add botton navigation bar

--- app/src/main/java/kinopoisk/cinema/di/module/ViewModelModule.kt
+++ app/src/main/java/kinopoisk/cinema/di/module/ViewModelModule.kt
@@ -1,6 +1,30 @@
 package kinopoisk.cinema.di.module
 
+import androidx.lifecycle.ViewModel
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import kinopoisk.cinema.di.ViewModelKey
+import kinopoisk.cinema.presentation.screen.homepage.HomeViewModel
+import kinopoisk.cinema.presentation.screen.profilepage.ProfileViewModel
+import kinopoisk.cinema.presentation.screen.searchpage.SearchViewModel
 
 @Module
-interface ViewModelModule
+interface ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(HomeViewModel::class)
+    fun bindHomeViewModel(viewModel: HomeViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SearchViewModel::class)
+    fun bindSearchViewModel(viewModel: SearchViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProfileViewModel::class)
+    fun bindProfileViewModel(viewModel: ProfileViewModel): ViewModel
+
+}
